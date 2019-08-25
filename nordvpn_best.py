fe364e167c5a5f8b0a2965e20715acd1ced139e3
Christopher Malau <chrismalau1@gmail.com>
2019-08-25 23:01:46 +1000

    remove australia hardcoded

--- nordvpn_best.py
+++ nordvpn_best.py
@@ -16,9 +16,12 @@ def get_servers(country: str, city: str = None):
     return filtered
 
 if __name__ == '__main__':
-    loc = input("Enter [CITY] [COUNTRY (optional)]: ").split()
+    loc = input("Enter [CITY] [COUNTRY]: ").split()
+    if len(loc) != 2:
+        print("Need two arguments")
+        exit(1)
     city = loc[0]
-    country = loc[1] if len(loc) == 2 else "Australia"
+    country = loc[1]
     servers = get_servers(country, city)
     x = [[s['name'], s['ip_address'], s['load'], (', '.join([cat['name'] for cat in s['categories']]))] for s in servers if int(s['load']) < MAX_LOAD]
     print(tabulate(x, headers=["Name", "IP", "Load %", "Categories"]))
